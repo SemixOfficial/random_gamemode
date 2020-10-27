2298a7247a45705cd113b3dd9212643e299d49bd
ASTEYA <58669707+ASTEYA49@users.noreply.github.com>
2020-10-26 21:00:59 -0700

    Update init.lua

--- random_gamemode/gamemode/init.lua
+++ random_gamemode/gamemode/init.lua
@@ -8,3 +8,31 @@ DEFINE_BASECLASS "gamemode_base";
 function GM:PlayerCanHearPlayersVoice(listener, talker)
 	return listener:GetPos():DistToSqr(talker:GetPos()) > 250000, true;
 end
+
+local RoundTime = 0.20
+local FreezeTime = 0.10
+local Delay = CurTime() + 60 * RoundTime
+local IsFreezeTime = false
+
+function GM:Tick()
+	local Timer = Delay - CurTime()
+	
+	if IsFreezeTime then
+		if Timer <= 0 then
+			IsFreezeTime = false
+			Delay = CurTime() + 60 * RoundTime
+		end
+	else
+		if Timer <= 0 then
+			IsFreezeTime = true
+			Delay = CurTime() + 60 * FreezeTime
+		end
+	end
+	
+	print( string.FormattedTime( Timer, '%2i:%02i' ), 'ft: ' .. tostring( IsFreezeTime ) )
+	
+	for Index, Human in pairs( player.GetHumans() ) do
+		Human:SetNWInt( 'RoundTime', RoundTime )
+		--Human:SetNWBool( 'IsFreezeTime', IsFreezeTime )
+	end
+end
